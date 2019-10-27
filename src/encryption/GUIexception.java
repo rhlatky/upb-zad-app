9bc58e20f4b24e386a1e739cd87a78332d8ea1ed
Mr-SvaRoq <bibo.dnt.159@gmail.com>
2019-10-27 18:55:47 +0100

    refactor - pridana funkcionalita, SheiBe

---
+++ src/encryption/GUIexception.java
@@ -0,0 +1,11 @@
+package encryption;
+
+public class GUIexception extends Exception {
+    public GUIexception(){
+
+    }
+
+    public GUIexception(String message){
+        super(message);
+    }
+}
--- src/encryption/RSAHandler.java
+++ src/encryption/RSAHandler.java
@@ -1,7 +1,5 @@
 package encryption;
 
-import org.apache.tomcat.util.codec.binary.Base64;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -9,7 +7,6 @@ import javax.crypto.NoSuchPaddingException;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 
 public class RSAHandler {
 	private Cipher cipher;
@@ -23,20 +20,9 @@ public class RSAHandler {
 		KeyFactory kf = KeyFactory.getInstance("RSA");
 		return kf.generatePrivate(spec);
 	}
-	
-	public PublicKey getPublic (byte[] publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
-		X509EncodedKeySpec spec = new X509EncodedKeySpec(publicKey);
-		KeyFactory kf = KeyFactory.getInstance("RSA");
-		return kf.generatePublic(spec);
-	}
-	
-	public String encryptText(byte[] msg, PublicKey key) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		this.cipher.init(Cipher.ENCRYPT_MODE, key);
-		return Base64.encodeBase64String(cipher.doFinal(msg));
-	}
 
 	public byte[] decryptText(byte [] msg, PrivateKey key) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		this.cipher.init(Cipher.DECRYPT_MODE, key);
-		return cipher.doFinal(Base64.decodeBase64(msg));
+		return cipher.doFinal(msg);
 	}
 }
